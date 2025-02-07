c8cdd5e5fef5796d1f7c10e04e5aa6957d51b614
Michael Smith <michaelsmith1994@yahoo.com>
2025-02-07 14:56:41 +0000

    another lab completed

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -5,9 +5,11 @@ import Lab.Repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class StoreService {
     StoreRepository storeRepository;
     @Autowired
@@ -20,14 +22,14 @@ public class StoreService {
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -36,13 +38,17 @@ public class StoreService {
      * @return a store entity
      */
     public Store getStoreById(long id){
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        if(optionalStore.isPresent()){
+            return optionalStore.get();
+        }
         return null;
     }
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
+        storeRepository.deleteById(id);;
     }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
@@ -50,6 +56,13 @@ public class StoreService {
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        if(optionalStore.isPresent()){
+            Store store = optionalStore.get();
+            store.setName(replacement.getName());
+            return storeRepository.save(store);
+
+        }
         return null;
     }
 
